cea18722f89bf48ac3e1faa2e56126f0eacedf1f
Diego Fonseca <dfonseca404@gmail.com>
2018-04-24 17:23:21 -0400

    Criar menu principal e tela de cadastro de aluno

--- GrupoX/src/br/edu/ifro/MenuPrincipalController.java
+++ GrupoX/src/br/edu/ifro/MenuPrincipalController.java
@@ -5,13 +5,18 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -26,6 +31,9 @@ public class MenuPrincipalController implements Initializable {
     private Button btnSoma;
     
     @FXML
+    private Label label;
+    
+    @FXML
     private void soma(ActionEvent event) {
         Double num1 = Double.parseDouble(txtNumero1.getText());
         Double num2 = Double.parseDouble(txtNumero2.getText());
@@ -38,5 +46,26 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(),900,682);
+            Stage stage = new Stage();
+            stage.setTitle("Cadastrar aluno");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(IOException e){
+
+        }
+    }
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+        
+    }
     
 }
